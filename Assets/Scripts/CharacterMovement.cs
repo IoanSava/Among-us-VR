11108e83c76f355cc923aca49c7924363ef909fc
Ioan <ioan.sava28@gmail.com>
2020-12-08 18:54:18 +0200

    feat: Player movement

---
+++ Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    Rigidbody rigidBody;
+    [SerializeField] float speed;
+
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        float x = Input.GetAxisRaw("Horizontal");
+        float z = Input.GetAxisRaw("Vertical");
+        Vector3 moveBy = transform.right * x + transform.forward * z;
+        rigidBody.MovePosition(transform.position + moveBy.normalized * speed * Time.deltaTime);
+
+        if (Input.GetAxis("Vertical") > 0 || Input.GetAxis("Horizontal") > 0)
+        {
+            GetComponent<Animator>().SetBool("isMoving", true);
+        }
+        else
+        {
+            GetComponent<Animator>().SetBool("isMoving", false);
+        }
+    }
+}
---
+++ Assets/Scripts/LookAround.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class LookAround : MonoBehaviour
+{
+    [SerializeField] float headRotationLimit = 90f;
+    [SerializeField] Transform cam;
+    [SerializeField] float sensitivity;
+    float headRotation = 0f;
+
+    void Start()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        float x = Input.GetAxis("Mouse X") * sensitivity * Time.deltaTime;
+        float y = Input.GetAxis("Mouse Y") * sensitivity * Time.deltaTime * -1f;
+        transform.Rotate(0f, x, 0f);
+        headRotation += y;
+        cam.localEulerAngles = new Vector3(headRotation, 0f, 0f);
+        headRotation += y;
+        headRotation = Mathf.Clamp(headRotation, -headRotationLimit, headRotationLimit);
+        cam.localEulerAngles = new Vector3(headRotation, 0f, 0f);
+    }
+}
