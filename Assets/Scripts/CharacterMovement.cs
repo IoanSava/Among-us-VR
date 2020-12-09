6da29be10f6ad3bfeb4382c19c6b0dd6b1926fda
csum112 <cataremix15@gmail.com>
2020-12-09 14:23:51 +0200

    updated corupted files

--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -1,7 +1,6 @@
 ﻿using UnityEngine;
-using Photon.Pun;
 
-public class CharacterMovement : MonoBehaviourPun
+public class CharacterMovement : MonoBehaviour
 {
     Rigidbody rigidBody;
     [SerializeField] float speed;
@@ -13,20 +12,12 @@ public class CharacterMovement : MonoBehaviourPun
 
     void Update()
     {
-<<<<<<< HEAD
         float x = Input.GetAxisRaw("Horizontal");
         float z = Input.GetAxisRaw("Vertical");
-<<<<<<< HEAD
         Vector3 moveBy = transform.right * z * -1 + transform.forward * x;
         rigidBody.MovePosition(transform.position + moveBy.normalized * speed * Time.deltaTime);
 
         if (Input.GetAxis("Vertical") != 0 || Input.GetAxis("Horizontal") != 0)
-=======
-        Vector3 moveBy = transform.right * x + transform.forward * z;
-        rigidBody.MovePosition(transform.position + moveBy.normalized * speed * Time.deltaTime);
-
-        if (Input.GetAxis("Vertical") > 0 || Input.GetAxis("Horizontal") > 0)
->>>>>>> 871a754... merged with character_movements
         {
             GetComponent<Animator>().SetBool("isMoving", true);
         }
@@ -34,22 +25,5 @@ public class CharacterMovement : MonoBehaviourPun
         {
             GetComponent<Animator>().SetBool("isMoving", false);
         }
-=======
-		if (photonView.IsMine == false && PhotonNetwork.IsConnected == true) {
-        	float x = Input.GetAxisRaw("Horizontal");
-        	float z = Input.GetAxisRaw("Vertical");
-        	Vector3 moveBy = transform.right * x + transform.forward * z;
-        	rigidBody.MovePosition(transform.position + moveBy.normalized * speed * Time.deltaTime);
-
- 	       if (Input.GetAxis("Vertical") > 0 || Input.GetAxis("Horizontal") > 0)
-     	   {
-      	      GetComponent<Animator>().SetBool("isMoving", true);
-      	  	}
-      	  	else
-       	 	{
-            	GetComponent<Animator>().SetBool("isMoving", false);
-        	}
-		}
->>>>>>> 42b2f74... added photonview
     }
 }
--- Assets/Scripts/LookAround.cs
+++ Assets/Scripts/LookAround.cs
@@ -1,7 +1,6 @@
 ﻿using UnityEngine;
-using Photon.Pun;
 
-public class LookAround : MonoBehaviourPun
+public class LookAround : MonoBehaviour
 {
     [SerializeField] float headRotationLimit = 90f;
     [SerializeField] Transform cam;
@@ -16,32 +15,11 @@ public class LookAround : MonoBehaviourPun
 
     void Update()
     {
-<<<<<<< HEAD
         float x = Input.GetAxis("Mouse X") * sensitivity * Time.deltaTime;
         float y = Input.GetAxis("Mouse Y") * sensitivity * Time.deltaTime * -1f;
         transform.Rotate(0f, x, 0f);
         headRotation += y;
-<<<<<<< HEAD
         headRotation = Mathf.Clamp(headRotation, -headRotationLimit, headRotationLimit);
         cam.localEulerAngles = new Vector3(headRotation, cam.localEulerAngles.y, cam.localEulerAngles.z);
-=======
-        cam.localEulerAngles = new Vector3(headRotation, 0f, 0f);
-        headRotation += y;
-        headRotation = Mathf.Clamp(headRotation, -headRotationLimit, headRotationLimit);
-        cam.localEulerAngles = new Vector3(headRotation, 0f, 0f);
->>>>>>> 871a754... merged with character_movements
-=======
-        if (photonView.IsMine == false && PhotonNetwork.IsConnected == true)
-        {
-            float x = Input.GetAxis("Mouse X") * sensitivity * Time.deltaTime;
-            float y = Input.GetAxis("Mouse Y") * sensitivity * Time.deltaTime * -1f;
-            transform.Rotate(0f, x, 0f);
-            headRotation += y;
-            cam.localEulerAngles = new Vector3(headRotation, 0f, 0f);
-            headRotation += y;
-            headRotation = Mathf.Clamp(headRotation, -headRotationLimit, headRotationLimit);
-            cam.localEulerAngles = new Vector3(headRotation, 0f, 0f);
-        }
->>>>>>> 42b2f74... added photonview
     }
 }
