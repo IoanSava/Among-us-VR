1909ca48cf24980a7d3f0db5d2dd28f85502b8c4
csum112 <cataremix15@gmail.com>
2020-12-09 14:18:52 +0200

    rebased player_spawning

--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -14,10 +14,17 @@ public class CharacterMovement : MonoBehaviour
     {
         float x = Input.GetAxisRaw("Horizontal");
         float z = Input.GetAxisRaw("Vertical");
+<<<<<<< HEAD
         Vector3 moveBy = transform.right * z * -1 + transform.forward * x;
         rigidBody.MovePosition(transform.position + moveBy.normalized * speed * Time.deltaTime);
 
         if (Input.GetAxis("Vertical") != 0 || Input.GetAxis("Horizontal") != 0)
+=======
+        Vector3 moveBy = transform.right * x + transform.forward * z;
+        rigidBody.MovePosition(transform.position + moveBy.normalized * speed * Time.deltaTime);
+
+        if (Input.GetAxis("Vertical") > 0 || Input.GetAxis("Horizontal") > 0)
+>>>>>>> 871a754... merged with character_movements
         {
             GetComponent<Animator>().SetBool("isMoving", true);
         }
--- Assets/Scripts/LookAround.cs
+++ Assets/Scripts/LookAround.cs
@@ -19,7 +19,14 @@ public class LookAround : MonoBehaviour
         float y = Input.GetAxis("Mouse Y") * sensitivity * Time.deltaTime * -1f;
         transform.Rotate(0f, x, 0f);
         headRotation += y;
+<<<<<<< HEAD
         headRotation = Mathf.Clamp(headRotation, -headRotationLimit, headRotationLimit);
         cam.localEulerAngles = new Vector3(headRotation, cam.localEulerAngles.y, cam.localEulerAngles.z);
+=======
+        cam.localEulerAngles = new Vector3(headRotation, 0f, 0f);
+        headRotation += y;
+        headRotation = Mathf.Clamp(headRotation, -headRotationLimit, headRotationLimit);
+        cam.localEulerAngles = new Vector3(headRotation, 0f, 0f);
+>>>>>>> 871a754... merged with character_movements
     }
 }
