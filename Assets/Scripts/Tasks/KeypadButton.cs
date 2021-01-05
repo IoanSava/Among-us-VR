1fd0eece078dcc78e612a6dd2ad50c5e0b35e390
csum112 <cataremix15@gmail.com>
2021-01-05 21:59:24 +0200

    feat: added keypad task

---
+++ Assets/Scripts/Tasks/KeypadButton.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class KeypadButton : MonoBehaviour
+{
+    [SerializeField] private KeypadTask keypad;
+
+    private void OnMouseOver()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            keypad.keypadEvent.Invoke(gameObject.name);
+        }
+    }
+}
